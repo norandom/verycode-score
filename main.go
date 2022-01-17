dff5ba76f28c718a180bd247a5658d40a852515f
Marius Ciepluch <11855163+norandom@users.noreply.github.com>
2022-01-17 20:03:08 +0100

    Policy file management

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 	flag "github.com/spf13/pflag"
+	
+	"path/filepath"
 
 )
 
 var (
-	token, message			string
-	dryRun, versionFlag     	bool
+	poliy				string					 	// security policy for scoring 
+	token				string 						// GH access token
+	message				string						// for output handling
+	dryRun, versionFlag     	bool						// for CI testing
 )
 
 func main() {
@@ -23,19 +27,21 @@ func main() {
 		return
 	}
 
+	// used in CI
 	if dryRun {
 		printOutput("dryRun: " + message)
 		return
 	}
 
 
-	// code here
+	// scorer code here
 	printOutput("Result: " + message)
 }
 
 func parseAndValidateInput() {
-	flag.StringVar(&message, "message", "", "i/o for dryRun")
-	flag.StringVar(&token, "token", "123", "Personal access token, repo scope")
+	flag.StringVar(&message, "message", "", "i/o for dry runs")
+	flag.StringVar(&token, "token", "123", "GH personal access token, repo scope needed")
+	flag.StringVar(&policy, "policy", "policies/standard.json", "policy for scoring")
 	flag.BoolVar(&dryRun, "dryRun", false, "for testing only")
 	flag.BoolVar(&versionFlag, "version", false, "output the version of verycode")
 	flag.Parse()
@@ -51,6 +57,13 @@ func parseAndValidateInput() {
 	var err error
 
 	if !dryRun {
+		
+		policyPath, err = filepath.FromSlash(policy)
+		if err != nil {
+			_, _ = fmt.Fprint(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		
 		if token == "" {
 			err = multierror.Append(err, errors.New("--token can't be empty"))
 		}
